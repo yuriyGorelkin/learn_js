25562796f16d3c4e8c33d3e3e437da4ff2b14add
yuriyGorelkin <Yuriygorelkin@gmail.com>
2018-09-30 16:02:47 +0300

    third commit

--- src/index/index.js
+++ src/index/index.js
@@ -1,1 +1,2 @@
+import './scripts/base.js';
 import './index.scss';
---
+++ src/index/scripts/base.js
@@ -0,0 +1,17 @@
+/*let x = +prompt ('Please enter the value X', '');
+let n = +prompt ('Please enter the value N', '');
+
+function pow(x, n) {
+  let result = x;
+
+  for (let i = 1; i < n; i++) {
+    result *= x;
+  }
+
+  return result;
+}
+
+alert( pow(x, n) );*/
+
+const button = document.querySelector('#btnToggleModal');
+console.log (button);
