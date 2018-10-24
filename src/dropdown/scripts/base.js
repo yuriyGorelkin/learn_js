c8a3c0d72645820e02a98c7dae83400a088754c4
yuriyGorelkin <Yuriygorelkin@gmail.com>
2018-10-24 22:01:58 +0300

    add dynamic elements

--- src/dropdown/scripts/base.js
+++ src/dropdown/scripts/base.js
@@ -1,21 +1,21 @@
 /**
- * @param {HTMLElement} el 
+ * @param {HTMLElement} widgetElement 
  * @param {Array} optionsList 
- */
+ * */
 
-function dropdown(el, optionsList) {
-    const titleEl = el.querySelector('.dropdown__title');
-    let isOpened = el.classList.contains('opened');
+function dropdown(widgetElement, optionsList) {
+    let isOpened = widgetElement.classList.contains('opened');
+    let titleEl;
 
-    function close() {        
+    function close() {
         isOpened = false;
-        el.classList.remove('opened');
+        widgetElement.classList.remove('opened');
         document.removeEventListener('click', close);
     }
 
     function open() {
         isOpened = true;
-        el.classList.add('opened');
+        widgetElement.classList.add('opened');
         document.addEventListener('click', close);
     }
 
@@ -23,29 +23,72 @@ function dropdown(el, optionsList) {
         e.stopPropagation();
 
         if (isOpened) {
-            setTitle (e.target.textContent);
             close();
+            setTitle(e.target.textContent);
         } else {
             open();
         }
     }
 
-    function setTitle (text) {
+    function setTitle(text) {
         titleEl.textContent = text;
     }
 
-    el.addEventListener('click', (e) => {
-        toggle(e);
-    });
+    function renderTitle() {
+        titleEl = document.createElement('div');
 
-    el.addEventListener('keypress', (e) => {
-        if (e.keyCode === 13) {
+        titleEl.classList.add('dropdown__title');
+        titleEl.textContent = optionsList[0];
+        titleEl.tabIndex = 0;
+        widgetElement.appendChild(titleEl);
+    }
+
+    function renderOptions() {
+        const optionsEl = document.createElement('ul');
+
+        optionsEl.classList.add('dropdown__options');
+        optionsList.forEach(
+            (optionText) => {
+                const optionEl = document.createElement('li');
+
+                optionEl.classList.add('dropdown__option');
+                optionEl.tabIndex = 0;
+                optionEl.textContent = optionText;
+
+                optionsEl.appendChild(optionEl);
+            });
+        widgetElement.appendChild(optionsEl);
+    }
+
+    function renderWidget() {
+        widgetElement.classList.add('dropdown');
+        renderTitle();
+        renderOptions();
+    }
+
+    function attachEvents() {
+        widgetElement.addEventListener('click', (e) => {
             toggle(e);
-        }
-    });
+        });
+
+        widgetElement.addEventListener('keypress', (e) => {
+            if (e.keyCode === 13) {
+                toggle(e);
+            }
+        });
+    }
+
+    function init() {
+        renderWidget();
+        attachEvents();
+    }
+
+    init();
 }
 
 const moviesList = ['Shrek', 'Lost', 'Star Wars'];
+const countries = ['USA', 'Germany', 'Ukraine'];
 
-dropdown(document.querySelector('#moviesDropdownMenu'));
-dropdown(document.querySelector('#countriesDropdownMenu'));
+
+dropdown(document.querySelector('#moviesDropdownMenu'), moviesList);
+dropdown(document.querySelector('#countriesDropdownMenu'), countries);
